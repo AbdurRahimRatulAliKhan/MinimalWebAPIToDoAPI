ba5025a8da7edc1c0f43bcbae9db0d8d18aed597
Ratul Ali <rahim498617@gmail.com>
2022-03-31 17:14:54 +0600

    Updated minimal web APIs Using JsonOptions

--- MinimalWebAPIToDoAPI/Program.cs
+++ MinimalWebAPIToDoAPI/Program.cs
@@ -1,92 +1,20 @@
-using Microsoft.EntityFrameworkCore;
+using System.Text.Json;
 
 var builder = WebApplication.CreateBuilder(args);
-//builder.Services.AddDatabaseDeveloperPageExceptionFilter();
-builder.Services.AddDbContext<TodoDb>(opt => opt.UseInMemoryDatabase("TodoList"));
-
 var app = builder.Build();
 
+var options = new JsonSerializerOptions(JsonSerializerDefaults.Web);
 
-
-app.MapGet("/todoitems", async (TodoDb db) =>
-
-    await db.Todos.Select(x => new ToDoItemDTO(x)).ToListAsync());
-
-app.MapGet("/todoitems/{id}", async (int id, TodoDb db) =>
-    await db.Todos.FindAsync(id)
-        is Todo todo
-            ? Results.Ok(new ToDoItemDTO(todo))
-            : Results.NotFound());
-
-app.MapPost("/todoitems", async (ToDoItemDTO todoItemDTO , TodoDb db) =>
+app.MapGet("/", () => Results.Json(new Todo
 {
-    var todoItem = new Todo
-    {
-        IsComplete = todoItemDTO.IsComplete,
-        Name = todoItemDTO.Name
-    };
-    db.Todos.Add(todoItem);
-    await db.SaveChangesAsync();
-
-    return Results.Created($"/todoitems/{todoItem.Id}", new ToDoItemDTO(todoItem));
-
-});
-
-app.MapPut("/todoitems/{id}", async (int id, ToDoItemDTO toDoItemDTO, TodoDb db) =>
-{
-    var todo = await db.Todos.FindAsync(id);
-
-    if (todo is null) return Results.NotFound();
-
-    todo.Name = toDoItemDTO.Name;
-    todo.IsComplete = toDoItemDTO.IsComplete;
-
-    await db.SaveChangesAsync();
-
-    return Results.NoContent();
-});
-
-app.MapDelete("/todoitems/{id}", async (int id, TodoDb db) =>
-{
-    if (await db.Todos.FindAsync(id) is Todo todo)
-    {
-        db.Todos.Remove(todo);
-        await db.SaveChangesAsync();
-        return Results.Ok(new ToDoItemDTO(todo));
-    }
-
-    return Results.NotFound();
-});
+    Name = "Walk dog",
+    IsComplete = false
+}, options));
 
 app.Run();
 
-public class Todo
+class Todo
 {
-    public int Id { get; set; }
     public string? Name { get; set; }
     public bool IsComplete { get; set; }
-    public string? Secret { get; set; }
 }
-
-public class ToDoItemDTO
-{
-    public int Id { get; set; }
-    public string? Name { get; set; }
-    public bool IsComplete { get; set; }
-
-    public ToDoItemDTO() { }
-    public ToDoItemDTO(Todo todoItem) =>
-        (Id, Name, IsComplete) = (todoItem.Id, todoItem.Name, todoItem.IsComplete);
-}
-
-class TodoDb : DbContext
-{
-    public TodoDb(DbContextOptions<TodoDb> options)
-        : base(options) { }
-
-    public DbSet<Todo> Todos => Set<Todo>();
-}
-
-
-
-
