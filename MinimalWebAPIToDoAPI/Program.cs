76a6490b2de06865f19ada51263e8ba625886ae5
Ratul Ali <rahim498617@gmail.com>
2022-03-31 17:10:57 +0600

    Updated minimal web APIs v2

--- MinimalWebAPIToDoAPI/Program.cs
+++ MinimalWebAPIToDoAPI/Program.cs
@@ -1,103 +1,45 @@
-//var builder = WebApplication.CreateBuilder(args);
-
-//// Add services to the container.
-
-////builder.Services.AddControllers();
-//// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-//builder.Services.AddEndpointsApiExplorer();
-//builder.Services.AddSwaggerGen();
-
-//var app = builder.Build();
-
-//// Configure the HTTP request pipeline.
-//if (app.Environment.IsDevelopment())
-//{
-//    app.UseSwagger();
-//    app.UseSwaggerUI();
-//}
-
-//app.UseHttpsRedirection();
-
-//var summaries = new[]
-//{
-//    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-//};
-
-//app.MapGet("/weatherforecast", () =>
-//{
-//    var forecast = Enumerable.Range(1, 5).Select(index =>
-//        new WeatherForecast
-//        (
-//            DateTime.Now.AddDays(index),
-//            Random.Shared.Next(-20, 55),
-//            summaries[Random.Shared.Next(summaries.Length)]
-//            ))
-//    .ToArray();
-//    return forecast;
-//})
-//    .WithName("GetWeatherForecast");
-
-////app.UseAuthorization();
-
-////app.MapControllers();
-
-//app.Run();
-
-//internal record WeatherForecast(DateTime Date, int TemperatureC, string? Summary)
-//{
-//    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-//}
-
-
-
-
-
-//var builder = WebApplication.CreateBuilder(args);
-//var app = builder.Build();
-
-//app.MapGet("/", () => "Hello World!");
-
-//app.Run();
-
-
-
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddDbContext<TodoDb>(opt => opt.UseInMemoryDatabase("TodoList"));
 //builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+builder.Services.AddDbContext<TodoDb>(opt => opt.UseInMemoryDatabase("TodoList"));
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+
 
 app.MapGet("/todoitems", async (TodoDb db) =>
-    await db.Todos.ToListAsync());
 
-app.MapGet("/todoitems/complete", async (TodoDb db) =>
-    await db.Todos.Where(t => t.IsComplete).ToListAsync());
+    await db.Todos.Select(x => new ToDoItemDTO(x)).ToListAsync());
 
 app.MapGet("/todoitems/{id}", async (int id, TodoDb db) =>
     await db.Todos.FindAsync(id)
         is Todo todo
-            ? Results.Ok(todo)
+            ? Results.Ok(new ToDoItemDTO(todo))
             : Results.NotFound());
 
-app.MapPost("/todoitems", async (Todo todo, TodoDb db) =>
+app.MapPost("/todoitems", async (ToDoItemDTO todoItemDTO , TodoDb db) =>
 {
-    db.Todos.Add(todo);
+    var todoItem = new Todo
+    {
+        IsComplete = todoItemDTO.IsComplete,
+        Name = todoItemDTO.Name
+    };
+    db.Todos.Add(todoItem);
     await db.SaveChangesAsync();
 
-    return Results.Created($"/todoitems/{todo.Id}", todo);
+    return Results.Created($"/todoitems/{todoItem.Id}", new ToDoItemDTO(todoItem));
+
 });
 
-app.MapPut("/todoitems/{id}", async (int id, Todo inputTodo, TodoDb db) =>
+app.MapPut("/todoitems/{id}", async (int id, ToDoItemDTO toDoItemDTO, TodoDb db) =>
 {
     var todo = await db.Todos.FindAsync(id);
 
     if (todo is null) return Results.NotFound();
 
-    todo.Name = inputTodo.Name;
-    todo.IsComplete = inputTodo.IsComplete;
+    todo.Name = toDoItemDTO.Name;
+    todo.IsComplete = toDoItemDTO.IsComplete;
 
     await db.SaveChangesAsync();
 
@@ -110,7 +52,7 @@ app.MapDelete("/todoitems/{id}", async (int id, TodoDb db) =>
     {
         db.Todos.Remove(todo);
         await db.SaveChangesAsync();
-        return Results.Ok(todo);
+        return Results.Ok(new ToDoItemDTO(todo));
     }
 
     return Results.NotFound();
@@ -118,7 +60,7 @@ app.MapDelete("/todoitems/{id}", async (int id, TodoDb db) =>
 
 app.Run();
 
-class Todo
+public class Todo
 {
     public int Id { get; set; }
     public string? Name { get; set; }
@@ -126,14 +68,14 @@ class Todo
     public string? Secret { get; set; }
 }
 
-class ToDoItemDTO
+public class ToDoItemDTO
 {
     public int Id { get; set; }
     public string? Name { get; set; }
     public bool IsComplete { get; set; }
 
     public ToDoItemDTO() { }
-    public ToDoItemDTO(Todo todoItem) => 
+    public ToDoItemDTO(Todo todoItem) =>
         (Id, Name, IsComplete) = (todoItem.Id, todoItem.Name, todoItem.IsComplete);
 }
 
@@ -144,3 +86,7 @@ class TodoDb : DbContext
 
     public DbSet<Todo> Todos => Set<Todo>();
 }
+
+
+
+
